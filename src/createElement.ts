27f70249bf2dece61f3be312b4400828fde511b4
Ubugeeei <ubuge1122@gamil.com>
2022-03-13 15:03:30 +0900

    feat: create virtual node element function

---
+++ src/createElement.ts
@@ -0,0 +1,56 @@
+import { KeyAttribute, VirtualNodeType } from "./vNode.interface";
+
+const TEXT_NODE = 3;
+
+/**
+ * create element function h
+ */
+export const h = (
+	name: VirtualNodeType["name"],
+	props: VirtualNodeType["props"],
+	children: (VirtualNodeType | string)[],
+	realNode?: VirtualNodeType["realNode"]
+) => {
+	const VNodeChildren: VirtualNodeType[] = [];
+
+	children.forEach((it) => {
+		if (typeof it === "string") {
+			const textVNode = createTextVNode(it);
+			VNodeChildren.push(textVNode);
+		} else {
+			VNodeChildren.push(it);
+		}
+	});
+
+	const vNode = createVNode(
+		name,
+		props,
+		VNodeChildren,
+		realNode,
+		null,
+		props.key
+	);
+
+	return vNode;
+};
+
+const createVNode = (
+	name: VirtualNodeType["name"],
+	props: VirtualNodeType["props"],
+	children: VirtualNodeType["children"],
+	realNode?: VirtualNodeType["realNode"],
+	nodeType?: VirtualNodeType["nodeType"],
+	key?: KeyAttribute
+): VirtualNodeType => ({
+	name,
+	props,
+	children,
+	realNode: realNode ? realNode : null,
+	nodeType: nodeType ? nodeType : null,
+	key: key ? key : null,
+});
+
+const createTextVNode = (
+	name: string,
+	realNode?: VirtualNodeType["realNode"]
+) => createVNode(name, {}, [], realNode, TEXT_NODE);
