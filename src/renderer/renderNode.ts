9e573d78e9ca4796f63daae1afe51e81c32546a7
Ubugeeei <ubuge1122@gamil.com>
2022-03-13 18:29:53 +0900

    feat: impl render text node

--- src/renderer/renderNode.ts
+++ src/renderer/renderNode.ts
@@ -1,3 +1,4 @@
+import { TEXT_NODE } from "../createElement";
 import { VirtualNodeType } from "../vNode.interface";
 
 export const renderNode = (
@@ -5,4 +6,36 @@ export const renderNode = (
 	realNode: VirtualNodeType["realNode"],
 	oldVNode: VirtualNodeType | null,
 	newVNode: VirtualNodeType
-) => {};
+) => {
+	if (newVNode === oldVNode) return;
+
+	if (
+		oldVNode &&
+		newVNode.nodeType === TEXT_NODE &&
+		oldVNode.nodeType === TEXT_NODE
+	) {
+		realNode = renderTextNode(realNode, newVNode);
+	}
+};
+
+const renderTextNode = (
+	realNode: VirtualNodeType["realNode"],
+	newVNode: VirtualNodeType
+) => {
+	if (!realNode) {
+		console.error(
+			"Error! rendering nodeType is TEXT_NODE, but realNode is null."
+		);
+		return realNode;
+	}
+
+	if (typeof newVNode.name === "string") {
+		realNode.nodeValue = newVNode.name;
+		return realNode;
+	} else {
+		console.error(
+			"Error!rendering nodeType is TEXT_NODE, but newNode.name is not string."
+		);
+		return realNode;
+	}
+};
