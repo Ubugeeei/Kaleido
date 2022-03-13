0a4f7a088f108a518a79d54fbbda5567ca75cf44
Ubugeeei <ubuge1122@gamil.com>
2022-03-13 15:03:42 +0900

    feat: define interfaces

---
+++ src/vNode.interface.d.ts
@@ -0,0 +1,36 @@
+export type TEXT_NODE = 3;
+
+export type KeyAttribute = string | number;
+
+export type DOMAttributeName = "key" | string;
+
+export interface DOMAttributes {
+	key?: KeyAttribute;
+	[prop: string]: any;
+}
+
+export interface HandlersType {
+	[eventName: string]: (event: Event) => void;
+}
+
+export type ElementAttachedNeedAttr = HTMLElement & {
+	vdom?: VirtualNodeType;
+	eventHandlers?: HandlersType;
+};
+
+export type TextAttachedVDom = Text & {
+	vdom?: VirtualNodeType;
+};
+
+export type ExpandElement =
+	| ElementAttachedNeedAttr
+	| TextAttachedVDom;
+
+export interface VirtualNodeType {
+	name: HTMLElementTagNameMap | string;
+	props: DOMAttributes;
+	children: VirtualNodeType[];
+	realNode: ExpandElement | null;
+	nodeType: TEXT_NODE | null;
+	key: KeyAttribute | null;
+}
