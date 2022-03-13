e4b9a27a350c0e47a6756fd3563cfb9429bd7d81
Ubugeeei <ubuge1122@gamil.com>
2022-03-13 18:21:00 +0900

    wip: impl render function

---
+++ src/renderer/index.ts
@@ -0,0 +1,23 @@
+import { createVNodeFromRealElement } from "../createVNode";
+import {
+	ElementAttachedNeedAttr,
+	VirtualNodeType,
+} from "../vNode.interface";
+import { renderNode } from "./renderNode";
+
+export const render = (
+	realNode: ElementAttachedNeedAttr,
+	newVNode: VirtualNodeType
+) => {
+	if (!realNode.parentElement) {
+		throw new Error("Error! realNode does not have parentNode.");
+	}
+
+	const vNode = createVNodeFromRealElement(realNode);
+	vNode.children = [newVNode];
+	newVNode = vNode;
+
+	const oldVNode = realNode.vdom ? realNode.vdom : { ...vNode };
+
+	renderNode(realNode.parentElement, realNode, oldVNode, newVNode);
+};
---
+++ src/renderer/renderNode.ts
@@ -0,0 +1,8 @@
+import { VirtualNodeType } from "../vNode.interface";
+
+export const renderNode = (
+	parentNode: HTMLElement,
+	realNode: VirtualNodeType["realNode"],
+	oldVNode: VirtualNodeType | null,
+	newVNode: VirtualNodeType
+) => {};
