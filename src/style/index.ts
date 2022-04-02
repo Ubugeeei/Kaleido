529890ed5e8398f76b6025e85018cf01d9caf170
Ubugeeei <ubuge1122@gamil.com>
2022-04-02 14:13:14 +0900

    feat: impl stylesheet

---
+++ src/style/index.ts
@@ -0,0 +1,23 @@
+interface StyleObjectInput {
+	[key: string]: { [property: string]: string };
+}
+
+interface StyleObject {
+	[key: keyof StyleObjectInput]: string;
+}
+
+export const ReactStyleSheet = {
+	create(o: StyleObjectInput): StyleObject {
+		const styles: StyleObject = {};
+
+		Object.keys(o).forEach((key) => {
+			styles[key] = "";
+			const style = o[key];
+			for (const [_key, value] of Object.entries(style)) {
+				styles[key] += `${_key}:\x20${value};\x20`;
+			}
+		});
+
+		return styles;
+	},
+};
