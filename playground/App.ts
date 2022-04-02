b71f959d44fe61eca2764884f0d2d58c5b188c76
Ubugeeei <ubuge1122@gamil.com>
2022-04-02 14:13:26 +0900

    feat: impl counter app style

--- playground/App.ts
+++ playground/App.ts
@@ -1,5 +1,6 @@
 import ReactDOM from "../src/react-dom/index";
 import { useEffect, useMemo, useState } from "../src/hooks/index";
+import { ReactStyleSheet } from "../src/style/index";
 import TodoApp from "./components/todo/TodoApp";
 
 const App = () => {
@@ -10,6 +11,11 @@ const App = () => {
 		console.debug("useMemo: count2Double calculated");
 		return count2 * 2;
 	}, [count2]);
+
+	const resetCounts = () => {
+		setCount(0);
+		setCount2(0);
+	};
 
 	useEffect(() => {
 		console.debug("useEffect: Effect only mounted");
@@ -22,43 +28,138 @@ const App = () => {
 	/**
 	 * render
 	 */
-	return ReactDOM.createElement("div", {}, [
-		ReactDOM.createElement("h3", {}, ["Counter App"]),
-		ReactDOM.createElement("p", {}, [
-			ReactDOM.createElement(
-				"span",
-				{ style: "margin-right: 15px" },
-				[`count: ${count}`]
-			),
-			ReactDOM.createElement(
-				"button",
-				{ onclick: () => setCount(count + 1) },
-				["+"]
-			),
-		]),
-
-		ReactDOM.createElement("p", {}, [
-			ReactDOM.createElement(
-				"span",
-				{ style: "margin-right: 15px" },
-				[`count2: ${count2}`]
-			),
-			ReactDOM.createElement(
-				"button",
-				{ onclick: () => setCount2(count2 + 1) },
-				["+"]
-			),
-			ReactDOM.createElement("span", { style: "margin-left: 15px" }, [
-				`double: ${count2Double}`,
+	return ReactDOM.createElement(
+		"div",
+		{ style: styles.appContainer },
+		[
+			ReactDOM.createElement("h3", {}, ["Counter App"]),
+			ReactDOM.createElement("div", { style: styles.flex }, [
+				ReactDOM.createElement(
+					"button",
+					{
+						onclick: () => setCount(count + 1),
+						style: styles.incrementButton + styles.mr4,
+					},
+					["+"]
+				),
+				ReactDOM.createElement("p", {}, [
+					ReactDOM.createElement("span", { style: styles.label }, [
+						"count: ",
+					]),
+					ReactDOM.createElement("span", {}, [`${count}`]),
+				]),
 			]),
-		]),
 
-		ReactDOM.createElement("hr", {}, []),
+			ReactDOM.createElement("div", { style: styles.flex }, [
+				ReactDOM.createElement(
+					"button",
+					{
+						onclick: () => setCount2(count2 + 1),
+						style: styles.mr4 + styles.incrementButton + styles.mr4,
+					},
+					["+"]
+				),
+				ReactDOM.createElement("p", {}, [
+					ReactDOM.createElement("span", { style: styles.label }, [
+						"count2: ",
+					]),
+					ReactDOM.createElement("span", {}, [`${count2}`]),
+				]),
+			]),
 
-		TodoApp(),
+			ReactDOM.createElement("div", { style: styles.flex }, [
+				ReactDOM.createElement(
+					"button",
+					{
+						style: styles.incrementButtonDisabled + styles.mr4,
+						disabled: true,
+					},
+					["+"]
+				),
+				ReactDOM.createElement("p", {}, [
+					ReactDOM.createElement("span", { style: styles.label }, [
+						"double: ",
+					]),
+					ReactDOM.createElement("span", {}, [`${count2Double}`]),
+				]),
+			]),
 
-		ReactDOM.createElement("hr", {}, []),
-	]);
+			ReactDOM.createElement(
+				"div",
+				{ style: styles.flex + styles.justifyEnd },
+				[
+					ReactDOM.createElement(
+						"button",
+						{ onClick: resetCounts, style: styles.resetCountButton },
+						["reset counts"]
+					),
+				]
+			),
+
+			ReactDOM.createElement("hr", {}, []),
+
+			TodoApp(),
+
+			ReactDOM.createElement("hr", {}, []),
+		]
+	);
 };
+
+export const THEME = {
+	primary: "#66f",
+	secondary: "#fa0",
+} as const;
+
+const styles = ReactStyleSheet.create({
+	appContainer: {
+		"max-width": "600px",
+		margin: "8rem auto",
+		padding: "1rem",
+		height: "100%",
+		"min-height": "600px",
+		background: "#fafafa",
+		color: "#444444",
+		border: "2px solid #ccc",
+		"border-radius": ".5rem",
+	},
+	incrementButton: {
+		height: "20px",
+		width: "20px",
+		border: "none",
+		background: THEME.primary,
+		color: "#fff",
+		"border-radius": ".25rem",
+	},
+	incrementButtonDisabled: {
+		height: "20px",
+		width: "20px",
+		border: "none",
+		background: "#ccc",
+		color: "#fff",
+		"border-radius": ".25rem",
+	},
+	resetCountButton: {
+		padding: ".5rem",
+		border: "none",
+		background: THEME.secondary,
+		color: "#fff",
+		"font-weight": "bold",
+		"border-radius": ".25rem",
+	},
+	label: {
+		"font-size": ".8rem",
+	},
+	// utils
+	flex: {
+		display: "flex",
+		"align-items": "center",
+	},
+	justifyEnd: {
+		"justify-content": "flex-end",
+	},
+	mr4: {
+		"margin-right": "1rem",
+	},
+});
 
 ReactDOM.render(App, document.getElementById("root-element"));
