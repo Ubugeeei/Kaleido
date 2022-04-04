92fe72769bdda49a1070b07f911f8368899ae4d2
Ubugeeei <ubuge1122@gamil.com>
2022-04-04 13:43:33 +0900

    reafactor: mutate real node

--- src/core/dom/render.ts
+++ src/core/dom/render.ts
@@ -23,7 +23,6 @@ export const render = (
 	vNode.children = [newVNode];
 	newVNode = vNode;
 
-	console.debug("loop");
 	renderNode(realNode.parentElement, realNode, oldVNode, newVNode);
 };
 
@@ -42,8 +41,9 @@ const renderNode = (
 		newVNode.nodeType === TEXT_NODE &&
 		oldVNode.nodeType === TEXT_NODE
 	) {
-		if (oldVNode.name === newVNode.name) return;
-		realNode = renderTextNode(realNode, newVNode);
+		if (oldVNode.name !== newVNode.name) {
+			realNode = renderTextNode(realNode, newVNode);
+		}
 	}
 
 	// render element
@@ -60,7 +60,144 @@ const renderNode = (
 
 	// mutate element
 	else if (realNode) {
-		realNode = mutateElement(realNode, oldVNode, newVNode);
+		realNode = updateOnlyThisNode(realNode, oldVNode, newVNode);
+		if (!realNode) throw new Error("realNode is null.");
+
+		// index for compare new VNode and old VNode
+		let oldChildCurrentIndex = 0;
+		let newChildCurrentIndex = 0;
+		const oldChildrenLength = oldVNode.children.length;
+		const newChildrenLength = newVNode.children.length;
+
+		const hasKeyOldChildren: Map<
+			KeyAttribute,
+			VirtualNodeType
+		> = new Map();
+
+		for (const child of oldVNode.children) {
+			const childKey = child.key;
+			if (!childKey) continue;
+			hasKeyOldChildren.set(childKey, child);
+		}
+
+		const renderedNewChildren: Map<
+			KeyAttribute,
+			"isRendered"
+		> = new Map();
+
+		while (newChildCurrentIndex < newChildrenLength) {
+			let oldChildVNode: VirtualNodeType | null;
+			let oldKey: string | number | null;
+			if (!oldVNode.children[oldChildCurrentIndex]) {
+				oldChildVNode = null;
+				oldKey = null;
+			} else {
+				oldChildVNode = oldVNode.children[oldChildCurrentIndex];
+				oldKey = oldChildVNode.key;
+			}
+			const newChildVNode = newVNode.children[newChildCurrentIndex];
+			const newKey = newChildVNode.key;
+
+			// 既にrenderされているoldChildVNodeをスキップする
+			if (
+				oldKey &&
+				renderedNewChildren.get(oldKey) === "isRendered"
+			) {
+				oldChildCurrentIndex++;
+				continue;
+			}
+
+			// keyを持っていない削除するべき要素を削除する
+			// ※keyを持っている削除するべき要素は最後にまとめて削除する
+			if (
+				newKey &&
+				oldChildVNode &&
+				oldChildVNode.children[oldChildCurrentIndex + 1] &&
+				newKey ===
+					oldChildVNode.children[oldChildCurrentIndex + 1].key
+			) {
+				// keyのない要素は以前のrenderの時と同じ位置になかったら削除する
+				if (!oldKey) {
+					realNode.removeChild(
+						oldChildVNode.realNode as ElementAttachedNeedAttr
+					);
+				}
+				oldChildCurrentIndex++;
+				continue;
+			}
+
+			// keyを持っていない子要素の更新処理
+			if (!newKey) {
+				if (!oldKey) {
+					renderNode(
+						realNode as ElementAttachedNeedAttr,
+						oldChildVNode ? oldChildVNode.realNode : null,
+						oldChildVNode,
+						newChildVNode
+					);
+					newChildCurrentIndex++;
+				}
+				oldChildCurrentIndex++;
+			} else {
+				// 以前のrender時とkeyが変わっていなかった場合、更新
+				if (oldChildVNode && oldKey === newKey) {
+					const childRealNode = oldChildVNode.realNode;
+					renderNode(
+						realNode as ElementAttachedNeedAttr,
+						childRealNode,
+						oldChildVNode,
+						newChildVNode
+					);
+					renderedNewChildren.set(newKey, "isRendered");
+					oldChildCurrentIndex++;
+				} else {
+					const previousRenderValue = hasKeyOldChildren.get(newKey);
+					// 以前のrender時には既にこのkeyを持つ要素が存在していた場合
+					if (previousRenderValue) {
+						renderNode(
+							realNode as ElementAttachedNeedAttr,
+							previousRenderValue.realNode,
+							previousRenderValue,
+							newChildVNode
+						);
+						renderedNewChildren.set(newKey, "isRendered");
+					}
+					// keyを持つ要素の追加処理
+					else {
+						renderNode(
+							realNode as ElementAttachedNeedAttr,
+							null,
+							null,
+							newChildVNode
+						);
+					}
+					renderedNewChildren.set(newKey, "isRendered");
+				}
+
+				newChildCurrentIndex++;
+			}
+		}
+
+		// 前のwhile処理で利用されなかった到達しなかったoldVNodeのindexの内keyを持っていないモノを削除
+		while (oldChildCurrentIndex < oldChildrenLength) {
+			const unReachOldVNode = oldVNode.children[oldChildCurrentIndex];
+
+			if (!unReachOldVNode.key && unReachOldVNode.realNode)
+				realNode.removeChild(unReachOldVNode.realNode);
+
+			oldChildCurrentIndex++;
+		}
+
+		// keyをもつoldVNodeの子要素の中で新しいVNodeでは削除されているものを削除
+		for (const [
+			oldKey,
+			oldChildVNode,
+		] of hasKeyOldChildren.entries()) {
+			if (!renderedNewChildren.get(oldKey)) {
+				const willRemoveNode = oldChildVNode.realNode;
+				if (willRemoveNode) realNode.removeChild(willRemoveNode);
+			}
+		}
 	}
 
 	if (realNode) {
@@ -89,149 +226,4 @@ const renderTextNode = (
 	realNode.nodeValue = newVNode.name;
 
 	return realNode;
-};
-
-const mutateElement = (
-	realNode: VirtualNodeType["realNode"],
-	oldVNode: VirtualNodeType,
-	newVNode: VirtualNodeType
-): VirtualNodeType["realNode"] => {
-	const newRealNode = updateOnlyThisNode(
-		realNode,
-		oldVNode,
-		newVNode
-	);
-
-	if (!newRealNode) throw new Error("realNode is null.");
-
-	// index for compare new VNode and old VNode
-	let oldChildCurrentIndex = 0;
-	let newChildCurrentIndex = 0;
-	const oldChildrenLength = oldVNode.children.length;
-	const newChildrenLength = newVNode.children.length;
-
-	const hasKeyOldChildren: Map<
-		KeyAttribute,
-		VirtualNodeType
-	> = new Map();
-
-	for (const child of oldVNode.children) {
-		const childKey = child.key;
-		if (!childKey) continue;
-		hasKeyOldChildren.set(childKey, child);
-	}
-
-	const renderedNewChildren: Map<
-		KeyAttribute,
-		"isRendered"
-	> = new Map();
-
-	while (newChildCurrentIndex < newChildrenLength) {
-		let oldChildVNode: VirtualNodeType | null;
-		let oldKey: string | number | null;
-		if (!oldVNode.children[oldChildCurrentIndex]) {
-			oldChildVNode = null;
-			oldKey = null;
-		} else {
-			oldChildVNode = oldVNode.children[oldChildCurrentIndex];
-			oldKey = oldChildVNode.key;
-		}
-		const newChildVNode = newVNode.children[newChildCurrentIndex];
-		const newKey = newChildVNode.key;
-
-		// 既にrenderされているoldChildVNodeをスキップする
-		if (oldKey && renderedNewChildren.get(oldKey) === "isRendered") {
-			oldChildCurrentIndex++;
-			continue;
-		}
-
-		// keyを持っていない削除するべき要素を削除する
-		// ※keyを持っている削除するべき要素は最後にまとめて削除する
-		if (
-			newKey &&
-			oldChildVNode &&
-			oldChildVNode.children[oldChildCurrentIndex + 1] &&
-			newKey === oldChildVNode.children[oldChildCurrentIndex + 1].key
-		) {
-			// keyのない要素は以前のrenderの時と同じ位置になかったら削除する
-			if (!oldKey) {
-				newRealNode.removeChild(
-					oldChildVNode.realNode as ElementAttachedNeedAttr
-				);
-			}
-			oldChildCurrentIndex++;
-			continue;
-		}
-
-		// keyを持っていない子要素の更新処理
-		if (!newKey) {
-			if (!oldKey) {
-				renderNode(
-					realNode as ElementAttachedNeedAttr,
-					oldChildVNode ? oldChildVNode.realNode : null,
-					oldChildVNode,
-					newChildVNode
-				);
-				newChildCurrentIndex++;
-			}
-			oldChildCurrentIndex++;
-		} else {
-			// 以前のrender時とkeyが変わっていなかった場合、更新
-			if (oldChildVNode && oldKey === newKey) {
-				const childRealNode = oldChildVNode.realNode;
-				renderNode(
-					realNode as ElementAttachedNeedAttr,
-					childRealNode,
-					oldChildVNode,
-					newChildVNode
-				);
-				renderedNewChildren.set(newKey, "isRendered");
-				oldChildCurrentIndex++;
-			} else {
-				const previousRenderValue = hasKeyOldChildren.get(newKey);
-				// 以前のrender時には既にこのkeyを持つ要素が存在していた場合
-				if (previousRenderValue) {
-					renderNode(
-						realNode as ElementAttachedNeedAttr,
-						previousRenderValue.realNode,
-						previousRenderValue,
-						newChildVNode
-					);
-					renderedNewChildren.set(newKey, "isRendered");
-				}
-				// keyを持つ要素の追加処理
-				else {
-					renderNode(
-						realNode as ElementAttachedNeedAttr,
-						null,
-						null,
-						newChildVNode
-					);
-				}
-				renderedNewChildren.set(newKey, "isRendered");
-			}
-
-			newChildCurrentIndex++;
-		}
-	}
-
-	// 前のwhile処理で利用されなかった到達しなかったoldVNodeのindexの内keyを持っていないモノを削除
-	while (oldChildCurrentIndex < oldChildrenLength) {
-		const unReachOldVNode = oldVNode.children[oldChildCurrentIndex];
-
-		if (!unReachOldVNode.key && unReachOldVNode.realNode)
-			newRealNode.removeChild(unReachOldVNode.realNode);
-
-		oldChildCurrentIndex++;
-	}
-
-	// keyをもつoldVNodeの子要素の中で新しいVNodeでは削除されているものを削除
-	for (const [oldKey, oldChildVNode] of hasKeyOldChildren.entries()) {
-		if (!renderedNewChildren.get(oldKey)) {
-			const willRemoveNode = oldChildVNode.realNode;
-			if (willRemoveNode) newRealNode.removeChild(willRemoveNode);
-		}
-	}
-
-	return newRealNode;
 };
