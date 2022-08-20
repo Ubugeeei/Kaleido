d86e5f9e5624db86c90c5f7653c4d2d23b80ef52
Ubugeeei <ubuge1122@gamil.com>
2022-08-20 23:54:17 +0900

    feat: read letter

--- src/tsx/parser.ts
+++ src/tsx/parser.ts
@@ -1,33 +1,91 @@
+
 class TSXTranspiler {
   private cursor: number
   private result: string
-  constructor(private source: string) {
+
+  constructor() {
     this.cursor = 0;
     this.result = '';
   }
 
-  transepile(): string {
-    this.skipWhitespace();
+  transpile(source: string): string {
+    this.skipWhitespace(source);
     return this.result;
   }
 
-  private skipWhitespace() {
-    while (this.cursor < this.source.length && this.source[this.cursor] === ' ') {
-      this.result += this.source[this.cursor];
+
+  letter(source: string): string | ParseError {
+    let result = '';
+    const reg_at_first = /[a-zA-Z]/;
+    const reg = /[a-zA-Z0-9_]/;
+    if (reg_at_first.test(source[this.cursor])) {
+      result += source[this.cursor];
+      this.cursor++;
+    } else {
+      const err = new ParseError(`letter: ${source.slice(this.cursor)}`);
+      console.error(err);
+      return err;
+    }
+
+    while (!this.end(source) && reg.test(source[this.cursor])) {
+      result += source[this.cursor];
       this.cursor++;
     }
+    return result;
+  }
+
+  private skipWhitespace(source: string) {
+    while (this.cursor < source.length && source[this.cursor] === ' ') {
+      this.result += source[this.cursor];
+      this.cursor++;
+    }
+  }
+
+  private end(source: string) {
+    return this.cursor >= source.length
+  }
+}
+
+class ParseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ParseError';
   }
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   it('transepile empty sourse', () => {
-    const tsxt = new TSXTranspiler('');
-    expect(tsxt.transepile()).toBe('');
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.transpile('')).toBe('');
+    }
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.transpile('  ')).toBe('  ');
+    }
   })
 
-  it('transepile blank sourse', () => {
-    const tsxt = new TSXTranspiler('  ');
-    expect(tsxt.transepile()).toBe('  ');
+  it('letter', () => {
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.letter('onClick')).toBe('onClick');
+    }
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.letter('onClick={countUp}')).toBe('onClick');
+    }
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.letter('onClick   ')).toBe('onClick');
+    }
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.letter(' onClick')).instanceof(ParseError);
+    }
+    {
+      const tsxt = new TSXTranspiler();
+      expect(tsxt.letter('={onClick')).instanceof(ParseError);
+    }
   })
 }
